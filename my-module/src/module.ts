06e5e6f31791d690f46c27b58262c347a8731e78
Peter Hauer <peter@newsadoo.com>
2025-07-08 13:20:53 +0200

    reproduction

---
+++ my-module/src/module.ts
@@ -0,0 +1,17 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'my-module',
+    configKey: 'myModule',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
+    _nuxt.options.css.push(resolver.resolve('./runtime/expose-me.scss'))
+  },
+})
---
+++ product/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <p class="container__elem">asdfasdf</p>
+  </div>
+</template>
